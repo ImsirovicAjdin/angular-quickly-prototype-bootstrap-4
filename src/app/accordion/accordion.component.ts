69ced29657d58b927dab243424e5e03f90bb848f
ImsirovicAjdin <ajdika@live.com>
2019-08-25 16:50:04 +0200

    Add ngx-bootstrap's accordion component

---
+++ src/app/accordion/accordion.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'demo-accordion-basic',
+  templateUrl: './accordion.component.html'
+})
+export class DemoAccordionBasicComponent {}
