24c1fdec20a05a3f9fae98799aba549f7ae06e6b
ImsirovicAjdin <ajdika@live.com>
2019-08-25 21:03:58 +0200

    Finish adding the accordion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DemoAccordionBasicComponent } from './accordion/accordion.component';
+import { AccordionModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, AccordionModule.forRoot(), BrowserAnimationsModule ],
+  declarations: [ AppComponent, HelloComponent, DemoAccordionBasicComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
